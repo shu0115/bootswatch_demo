96b8a1fe312127683fac3c42973f3be2a1ceccb1
shu0115 <s.matsumoto0115@gmail.com>
2014-03-06 17:48:10 +0900

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ BootswatchDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
