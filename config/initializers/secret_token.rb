c7bafae8247d117a1d220351192d49c47cbd4c9b
shu0115 <s.matsumoto0115@gmail.com>
2014-03-06 17:32:59 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootswatchDemo::Application.config.secret_key_base = 'b488fbe81e9109f0c1369b51295d098ac9fb8639961d8f1cfe18ea3689bce479b530d90d84a7deedb863ff6e5a10366a5f180c24def184870d6a6965bb2fea29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootswatchDemo::Application.config.session_store :cookie_store, key: '_bootswatch_demo_session'
